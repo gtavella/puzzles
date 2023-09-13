182a7945a6f8b83264b802189765e6d075f74346
Giuseppe Tavella <93888736+gtavella@users.noreply.github.com>
2023-09-13 19:57:17 +0200

    Update innermost_parenth.py

--- innermost_parenth.py
+++ innermost_parenth.py
@@ -1,5 +1,9 @@
 # capture the innermost parenthesis (..) [..] {..}
 # {{{{A}}{A}}{A}}{(A)}{{{}{A}}[A]}{{{{A}}}}{}{{{A}}{A}{{{A}}}}
+# USAGE
+# only use one parenthesis
+# ({Ca}3)3 -> NO
+# (Ga{Ca}3)3 -> YES
 
 import re
 
@@ -7,44 +11,58 @@ import re
 pattern_inner_parenth = re.compile(r"(\([^()[\]{}]+\))(\d*)|(\[[^[\](){}]+\])(\d*)|({[^{}()[\]]+})(\d*)")
 pattern_formula = re.compile(r"([A-Z][a-z]*)(\d*)")
 
-target = '(A(K4C(A(PPr2)3)3)5)2(B3[FeDe]2Gh2(He2D4))3(Ga2HHe4Hi)2'
-elements_map = {}
 
-match_indexes = []
+elements_map = {}
 
 
 
 # slices a string given a list of list of start, end indexes
-# def slice_str(target, indexes_list):
-#     # get elements index that are not requested in indexes_list
-#     count = 0
-#     indexes_wanted = []
-#     # if next start is bigger than current last, it means there's a gap of wanted indexes
-#     for i, indexes in enumerate(indexes_list):
-#         start, end = indexes
-#         last_end = end
-#         diff = start - last_end
-#         # add indexes count for
-#         for _ in range(0,diff):
-#             if diff > 0:
-#                 count += 1
-#                 indexes_wanted.append(count)
+def slice_str(target, indexes_list):
+    result_str = ''
+    n = 0
+    for i, indexes in enumerate(indexes_list):
+        start, end = indexes
         
-
-# slice_str('GiuseppeTavellaAtUnical', [[1,2], [4,8], [10,13], [16,19]])
-
- 
-    # return a target string with innermost parenthesis removed
+        # first indexes
+        if i == 0:
+            start = start
+            last_end = 0
+            n = last_end
+            while n < start:
+                # print('n: ', n)
+                result_str += target[n]
+                n += 1
+        else:
+            start = start
+            last_end = indexes_list[i-1][1]
+            n = last_end
+            while n < start:
+                # print('n: ', n)
+                result_str += target[n]
+                n += 1
     
-    # target = target[:start] + target[end:]
+        # if it is the last indexes, you need to repeat the process twice
+        if i == len(indexes_list)-1:
+            start = len(target)-1
+            last_end = end
+            n = last_end
+            while n <= start:
+                # print('n: ', n)
+                result_str += target[n]
+                n += 1
+            
+    return result_str
 
 
 
 
 
+target = '(Na(CH3)2)2[NaCa3(K3)]2'
 
-def simplify_formula(target):
-    print(target)
+
+def simplify_formula(target):        
+    match_indexes = []
+    # print(target)
     # search for innermost parenthesis
     for match_inner_parenth in pattern_inner_parenth.finditer(target):
         start, end = match_inner_parenth.span()
@@ -53,7 +71,7 @@ def simplify_formula(target):
         formula_parenth = match_inner_parenth.group(1) or match_inner_parenth.group(3) or match_inner_parenth.group(5)
         # if this is null, set to 1
         coeff_parenth = int(match_inner_parenth.group(2) or match_inner_parenth.group(4) or match_inner_parenth.group(6) or 1)
-        print('current parenthesis: ', target[start:end])
+        # print('group:        ', target[start:end])
 
         # if there's no match with parenthesis or coeff parenthesis is 1, then there's no parenthesis or nothing to multiply
         # if (not formula_parenth) or (coeff_parenth == 1):
@@ -69,14 +87,21 @@ def simplify_formula(target):
                     elements_map[element] += coeff_element_new
             else:
                 elements_map[element] = coeff_element_new
-            # print(element, coeff_element_new)
+            # print(element, coeff_element_new, coeff_parenth)
     
 
     # slices innermost parenthesis
-    # target = slice_str(target, match_indexes)
-    # return simplify_formula(target)
+    target = slice_str(target, match_indexes)
+
+    if target == '':
+        return target
+    else:
+        return simplify_formula(target)
+
+
+
 
 
 simplify_formula(target)
-
+print(elements_map)
         
